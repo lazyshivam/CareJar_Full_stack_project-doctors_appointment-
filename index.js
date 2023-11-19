c3403b12655fd073eb125bc6ddeaa8f5f96f6e5a
lazyshivam <shivamgoswami.ss.pp@gmail.com>
2023-11-19 21:50:12 +0530

    added dist file for rendering the build file using express

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-
 const express = require('express');
 const Chance = require('chance');
 const cors= require('cors');
 const app = express();
-const PORT = 8000;
+const path = require('path');
+
+require('dotenv').config();
+
+
+
+const PORT = process.env.PORT||8000;
 
 app.use(express.json());
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Handle requests to your React app
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
 // Create a new instance of Chance
 const chance = new Chance();
 
-
+// generating doctors info here 
 const generateDoctors = () => {
   const doctors = [];
   for (let i = 1; i <= 20; i++) {
@@ -39,12 +52,12 @@ const generateDoctors = () => {
   return doctors;
 };
 
-// Example usage
+
 const generatedDoctors = generateDoctors();
-console.log(generatedDoctors);
+// console.log(generatedDoctors);
 
 
-
+// gerenating random categories
 const generateCategories = () => {
   const categories = [];
   for (const category of ['Cardiologist', 'Dermatologist', 'Orthopedic', 'Pediatrician']) {
